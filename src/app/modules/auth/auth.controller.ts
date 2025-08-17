382a2eb8b5aece590fb41e479caac600ddf7eafa
mahedihsharif <mahedi5061@gmail.com>
2025-08-17 17:35:51 +0600

    update:auth

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -35,11 +35,11 @@ const credentialsLogin = catchAsync(
     passport.authenticate("local", async (err: any, user: any, info: any) => {
       if (!user) {
         if (info.type === IAuthError.NOT_FOUND) {
-          return next(new AppError(httpStatus.NOT_FOUND, info.message));
+          return next(new AppError(httpStatus.NOT_FOUND, info));
         } else if (info.type === IAuthError.NOT_VERIFIED) {
-          return next(new AppError(httpStatus.FORBIDDEN, info.message));
+          return next(new AppError(httpStatus.FORBIDDEN, info));
         } else if (info.type === IAuthError.INCORRECT_PASSWORD) {
-          return next(new AppError(httpStatus.UNAUTHORIZED, info.message));
+          return next(new AppError(httpStatus.UNAUTHORIZED, info));
         }
       }
 
